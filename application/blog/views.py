55fcae802bf797f48a26a2c10243e60d9115e558
Caoliang <i@caoliang.net>
2017-04-08 17:16:24 +0800

    add style

--- application/blog/views.py
+++ application/blog/views.py
@@ -1,4 +1,11 @@
 from wsgiserver.template import render
+from wsgiserver.http import HttpResponse
+
+
+def favicon_response(request):
+    with open("static/img/favicon.png", "rb") as f:
+        content = f.read()
+    return HttpResponse(content, content_type="image/x-icon")
 
 
 def home_page(request):
--- application/urls.py
+++ application/urls.py
@@ -1,7 +1,8 @@
 from application.blog.urls import url as blog_url_map
-from application.blog.views import home_page
+from application.blog.views import home_page, favicon_response
 
 url = {
     "^/$": home_page,
+    "^/favicon.ico/?$": favicon_response,
     "^/blog": blog_url_map,
 }
