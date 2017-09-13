20951ccdbe7138b2433769958a6403cda45a9bc2
Caoliang <i@caoliang.net>
2017-09-13 10:11:14 +0800

    add licence

--- madliar/__init__.py
+++ madliar/__init__.py
@@ -1,6 +1,15 @@
 """
-Configure the setup.py settings.
+    madliar
+    ~~~~~~~
 
+    Madliar is a wsgi web framework written in pure Python.
+    For more information - see `README.md`. A demo website
+    is <https://www.madliar.com>.
+
+    Caoliang
+    --------
+
+    :copyright: (c) 2017 MIT License
 """
 
 __version__ = '0.1a1.dev1076'
