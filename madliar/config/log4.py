0b7caf67e5fb9f1e810b58d2ed9998d25917574a
Caoliang <i@caoliang.net>
2017-09-14 17:12:25 +0800

    madliar sys log name.

--- madliar/config/log4.py
+++ madliar/config/log4.py
@@ -7,7 +7,7 @@ __all__ = ("logger", )
 
 logger = logging.getLogger("madliar")
 logger.setLevel(logging.DEBUG)
-fh = logging.FileHandler(os.path.join(settings.SYS_LOG_PATH, "madliar_debug.log"))
+fh = logging.FileHandler(os.path.join(settings.SYS_LOG_PATH, "madliar_sys.log"))
 formatter = logging.Formatter('%(levelname)s %(asctime)s %(filename)s:%(lineno)d:%(funcName)s %(message)s')
 fh.setFormatter(formatter)
 logger.addHandler(fh)
